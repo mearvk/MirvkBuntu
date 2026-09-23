71d61fff9951a7fa1d6abdd5c73f4e36cc4113e1
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-22 20:45:26 -0400

    theme: apply color inference configuration to GNOME profiles

--- build/build-desktop.sh
+++ build/build-desktop.sh
@@ -16,6 +16,7 @@ write_package_list "$WORK_DIR" mirvkbuntu-desktop
 stage_mirvkbuntu_source "$WORK_DIR"
 write_mirvkbuntu_manifest "$WORK_DIR"
 stage_native_outputs "$WORK_DIR"
+stage_color_inference "$WORK_DIR"
 run_live_build "$WORK_DIR"
 ISO_SOURCE="$(find_iso "$WORK_DIR")"
 mkdir -p "$OUTPUT_ROOT"
